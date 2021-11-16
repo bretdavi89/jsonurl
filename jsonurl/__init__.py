ea6af134aa856363a44cff9acc8809dabad24b39
Bret Davis <bret.davis@intel.com>
2021-11-16 01:31:44 +0000

    Fix import issue for 3.x

--- jsonurl/__init__.py
+++ jsonurl/__init__.py
@@ -1,1 +1,3 @@
-from jsonurl import parse_query, query_string, dict_to_args, args_to_dict
+from __future__ import absolute_import
+
+from jsonurl.jsonurl import parse_query, query_string, dict_to_args, args_to_dict
